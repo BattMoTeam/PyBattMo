5a0cff6861b69dba6df97673f9d6a88beb586254
Olav Møyner <olav.moyner@gmail.com>
2025-08-25 21:54:24 +0200

    Update script

--- examples/simulation.py
+++ examples/simulation.py
@@ -1,7 +1,8 @@
 # Partial port of https://battmoteam.github.io/BattMo.jl/dev/tutorials/2_run_a_simulation
-import battmo as bm
+from battmo import load_cell_parameters, load_cycling_protocol, LithiumIonBattery, Simulation, solve
 
-cell_parameters = bm.load_cell_parameters(from_default_set = "Chen2020")
-cycling_protocol = bm.load_cycling_protocol(from_default_set = "CCDischarge")
-model_setup = bm.LithiumIonBattery()
-output = bm.solve(sim)
+cell_parameters = load_cell_parameters(from_default_set = "Chen2020")
+cycling_protocol = load_cycling_protocol(from_default_set = "CCDischarge")
+model_setup = LithiumIonBattery()
+sim = Simulation(model_setup, cell_parameters, cycling_protocol)
+output = solve(sim)
